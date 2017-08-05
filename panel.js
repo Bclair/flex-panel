d14ca904ce73ef7e6bd012a16e21c8a975928ded
Bclair <bclair@gmail.com>
2017-08-05 01:18:43 -0400

    Add files via upload

---
+++ panel.js
@@ -0,0 +1,15 @@
+ const box = document.querySelectorAll('.box');
+
+    function toggleOpen() {
+      this.classList.toggle('open');
+    }
+
+    function toggleActive(e) {
+      console.log(e.propertyName);
+      if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+      }
+    }
+
+    box.forEach(box => box.addEventListener('click', toggleOpen));
+    box.forEach(box => box.addEventListener('transitionend', toggleActive));
